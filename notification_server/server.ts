235d2b9829278a2c267726973562381485a13c1b
bismuth01 <siddharthaswarnkar@gmail.com>
2025-07-12 11:27:30 +0530

    Base notification query

--- notification_server/server.ts
+++ notification_server/server.ts
@@ -1,10 +1,24 @@
-import express from "express";
+import express, {Request, Response} from "express";
+import { NotificationQuery } from "./types/NotificationQuery";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send(`Hello World!`);
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+    var data = req.params.one;
+    res.json(`Hello World!`);
+})
+
+app.get('/notification/:email', (req:Request<NotificationQuery>, res) => {
+    const query_details = req.params;
+    const email = query_details.email;
+    console.log(`Email received: ${email}`);
+    res.json({email});
 })
 
 app.listen(port, () => {
